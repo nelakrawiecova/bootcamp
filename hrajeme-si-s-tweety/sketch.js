abb86eeb2f32915d6b4dfe3052d068478c7f80dc
Nela Krawiecova <nela.krawiecova@gmail.com>
2018-06-29 11:43:23 +0200

    making changes

---
+++ hrajeme-si-s-tweety/sketch.js
@@ -0,0 +1,12 @@
+// do not remove, allowes autocomplete
+/// <reference path="./../p5.global-mode.d.ts" />
+
+function setup() {
+  var vysledek = tweets[0].text.replace("rep", "hip hop");
+
+  console.log(vysledek);
+}
+
+function draw() {
+  // put drawing code here
+}
--- rothko/empty-example/sketch.js
+++ rothko/empty-example/sketch.js
@@ -1,17 +1,49 @@
 // do not remove, allowes autocomplete
 /// <reference path="./../p5.global-mode.d.ts" />
 
+var clicks = 10;
+var keys = 10;
+var time = 0;
+var timespeed = 1;
+var myHue = 0;
+
 function setup() {
   createCanvas(400, 400);
+  angleMode(DEGREES);
+  colorMode(HSB, 100);
 
-  fill("pink");
+}
 
-  rect(10, 10, 100, 100);
+function draw() { 
+  time = time + timespeed;  
+  var ellipseOffset = time % 400;
+  var myHue = time % 100;
 
+  if (ellipseOffset > 300) {
+    background("red");
+  } else {
+    background(myHue, 100, 100);
+  }
   
-  // put setup code here
+
+  if (keyIsPressed) {
+    timespeed = 10;
+  } else {
+    timespeed = 1;
+  }
+  
+  ///stroke("pink");
+  fill("black");
+
+  ellipse(ellipseOffset, 200, clicks * 10, clicks * 10);
+  ellipse(300, 300, clicks * 10, clicks * 10);
 }
 
-function draw() {
-  // put drawing code here
+function mouseReleased() {
+  clicks = clicks + 1;
 }
+
+function keyReleased() {
+  keys = keys + 1;
+} 
+
